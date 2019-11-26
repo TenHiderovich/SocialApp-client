24d59cef399c2fe353f3707421c22c59e51ebcb6
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2019-11-26 08:52:50 +0800

    update: add redux, configurated store

---
+++ src/redux/store.js
@@ -0,0 +1,27 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import userReduser from './reducers/userReducer';
+import uiReduser from './reducers/uiReduser';
+import dataReduser from './reducers/dataReduser';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const reducers = combineReducers({
+    user: userReduser,
+    data: dataReduser,
+    UI: uiReduser
+});
+
+const store = createStore(
+    reducers, 
+    initialState, 
+    compose(
+        applyMiddleware(...middleware), 
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
+
+export default store;
